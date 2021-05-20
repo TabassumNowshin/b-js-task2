570356bc81362cb92a5055c8a9cf5270ee965bd5
Nowshin <60663074+TabassumNowshin@users.noreply.github.com>
2021-05-20 21:08:00 +0600

    added local storage

--- script.js
+++ script.js
@@ -52,14 +52,57 @@ class UI {
   static deleteFromBook(target){
     if (target.hasAttribute("href")) {
       target.parentElement.parentElement.remove();
+      Store.removeBook(target.parentElement.previousElementSibling.textContent.trim());
       UI.showAlert("Book Removed!", "success");
     }
+  }
+}
+
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem("books") === null) {
+      books = [];
+    }else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    let books = Store.getBooks();
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static displayBooks() {
+    let books = Store.getBooks();
+
+    books.forEach(book => {
+      UI.addToBookList(book);
+    });
+  }
+
+  static removeBook(isbn) {
+    let books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if(book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    })
+
+    localStorage.setItem("books", JSON.stringify(books));
+
   }
 }
 
 // add event listener
 form.addEventListener("submit", newBook);
 booklist.addEventListener("click", removeBook);
+document.addEventListener("DOMContentLoaded", Store.displayBooks());
 
 //functions
 
@@ -80,6 +123,8 @@ function newBook(e) {
     UI.clearFields();
     
     UI.showAlert("Book Added!", "success");
+
+    Store.addBook(book);
   }
 
 }
