ceaac967441c1446301dd80e1e76fb56652d2425
Nowshin <60663074+TabassumNowshin@users.noreply.github.com>
2021-05-20 18:11:25 +0600

    delete books

--- script.js
+++ script.js
@@ -1,38 +1,97 @@
-// Select all the buttons
-let btn1 = document.querySelector("#btn1");
-let btn2 = document.querySelector("#btn2");
-let btn3 = document.querySelector("#btn3");
-let btn4 = document.querySelector("#btn4");
-let btn5 = document.querySelector("#btn5");
+//OOP
 
-let list = document.querySelector("#cart-list");
+// get UI element
+let form = document.querySelector("#book-form");
+let booklist = document.querySelector("#book-list");
 
-// Add Event Listener
-btn1.addEventListener("click", addtoCart);
-btn2.addEventListener("click", addtoCart);
-btn3.addEventListener("click", addtoCart);
-btn4.addEventListener("click", addtoCart);
-btn5.addEventListener("click", addtoCart);
 
-list.addEventListener("click", rmitem);
-
-function addtoCart(e) {
-    let iname = e.target.parentElement.parentElement.children[0].innerHTML;
-    let iprice = e.target.parentElement.parentElement.children[1].innerHTML;
-
-    let item = document.createElement("tr");
-    item.innerHTML = `
-    <td>${iname}</td>
-    <td>${iprice}</td>
-    <td><button type="button">Remove</button></td>`;
-    list.appendChild(item);
+class Book {
+  constructor(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+  }
 }
 
-function rmitem(e){
-    if (e.target.hasAttribute("type")){
-        if(confirm("Are you sure?")){
-            let item = e.target.parentElement.parentElement;
-            item.remove();
-        }
+
+//UI class
+class UI {
+  constructor() {
+
+  }
+
+  addToBookList(book) {
+    let list = document.querySelector('#book-list');
+    let tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href='#' class='delete'>X</a></td>`;
+    list.appendChild(tr);
+  }
+
+  clearFields() {
+    document.querySelector('#title').value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector('#isbn').value = "";
+  }
+
+  showAlert(message, className) {
+    let div = document.createElement('div');
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+
+    let container = document.querySelector(".container");
+    let form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    setTimeout( () => {
+      document.querySelector(".alert").remove();
+    }, 3000); 
+  }
+
+  deleteFromBook(target){
+    if (target.hasAttribute("href")) {
+      target.parentElement.parentElement.remove();
     }
+  }
+}
+
+// add event listener
+form.addEventListener("submit", newBook);
+booklist.addEventListener("click", removeBook);
+
+//functions
+
+function newBook(e) {
+  e.preventDefault();
+
+  let title = document.querySelector('#title').value, author = document.querySelector("#author").value, isbn = document.querySelector('#isbn').value;
+
+  let ui = new UI();
+
+  if (title === '' || author === '' || isbn === '') {
+    ui.showAlert("Please fill up all the fields!", "error");    
+
+  } else {
+    
+    let book = new Book(title, author, isbn);
+
+    ui.addToBookList(book);
+    
+    ui.clearFields();
+    
+    ui.showAlert("Book Added!", "success");
+  }
+
+}
+
+function removeBook(e) {
+  e.preventDefault();
+
+  let ui = new UI;
+
+  ui.deleteFromBook(e.target);
+  ui.showAlert("Book Removed!", "success");
 }
